5ecb4fb4ad87a8a9614191ec0d2cc66acb9d6c9b
Svetlana Soboleva <sobolevasvetlanaalex@gmail.com>
2024-10-04 15:21:31 +0200

    :tada: start with portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import './header.css'
+
+export const Header = () => {
+  return (
+    <div className='header__block'>
+    <div className='title__block'>
+        <p>I am Svetlana</p>
+        <div className='wraper'>
+        <h1 className='title'>Full-Stack Developer</h1>
+        </div>  
+    </div>
+    <div className='image__block'>
+        <img src="../../public/—Pngtree—cute girl avatar element icon_4393286.png" alt="avatar" />
+    </div>
+    </div>
+  )
+}
